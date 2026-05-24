c59174fdfdd77bf6959a59c6c518bcb5dddbb97b
sarithagudur <42064481+sarithagudur@users.noreply.github.com>
2026-05-24 10:44:57 +0100

    Create Average_Speed.py

---
+++ Average_Speed.py
@@ -0,0 +1,19 @@
+a=10
+b=20
+c=30
+
+total=a+b+c
+mean= total/3
+
+if mean-a<0:
+    print("It is faster than the mean")
+else:
+    print("it is slower than the mean")
+if mean-b<0:
+    print("It is faster than the mean")
+else:
+    print("it is slower than the mean")
+if mean-c<0:
+    print("It is faster than the mean")
+else:
+    print("it is slower than the mean")
